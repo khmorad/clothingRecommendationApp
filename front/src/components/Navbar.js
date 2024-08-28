e3c57748355abaa80eae147d2f4d8f050c0df006
Shizuka <tkpp26@gmail.com>
2024-08-28 16:40:36 -0700

    Add logo

--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import "../stylings/Navbar.css";
 import Login from "./Login";
 
-
 export default function Navbar({
   showLogin,
   setShowLogin,
@@ -11,25 +10,31 @@ export default function Navbar({
   user,
   setUser,
   isEmployee,
-  setIsEmployee
+  setIsEmployee,
 }) {
   const handleGoogleAuthClick = () => {
     setShowLogin(true);
   };
 
   const handleLogout = () => {
-    localStorage.removeItem('username');
-    localStorage.removeItem('userType');
+    localStorage.removeItem("username");
+    localStorage.removeItem("userType");
     setLoginStatus(false);
     setUser(null);
     setIsEmployee(false);
     setShowLogin(false);
-    console.log('User logged out and local storage cleared');
+    console.log("User logged out and local storage cleared");
   };
 
   return (
     <nav className="navbar">
       <h3 className="logo" style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src="https://media.discordapp.net/attachments/1071334736787673188/1277778362319765544/DALLE_2024-08-26_16.54.14_-_A_logo_for_a_clothing_store_without_any_text_featuring_a_magnifying_glass_hovering_over_a_piece_of_womens_clothing._The_clothing_should_be_stylish_a.webp?ex=66d06195&is=66cf1015&hm=1df9c991b0c1436ab7ade847b94da952e25fad48b985a53c01cc70cbaac30a43&=&format=webp&width=993&height=993"
+          alt="Clothing Store Logo"
+          width="40"
+          style={{ marginRight: "10px" }}
+        />
         ClothePlus
       </h3>
       <ul className="nav-menu">
